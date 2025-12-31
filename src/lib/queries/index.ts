ce7bcca6de93cbc891cb9889f01e3f1ad15960a9
alexnetofit <kinhonetovai@gmail.com>
2025-12-31 20:30:27 -0300

    fix: atualizar exports em queries/index.ts

--- src/lib/queries/index.ts
+++ src/lib/queries/index.ts
@@ -1,3 +1,3 @@
-export { getUpcomingEventsWithFights, getUserStats } from './dashboard';
-export { getAllEventsWithCounts, getEventWithFights, getEventWithUserPicks } from './events';
-export { getGlobalRanking } from './ranking';
+export { getNextEventWithFights, getUserStats, getUserEventHistory } from './dashboard';
+export { getUpcomingEventsWithCounts, getEventWithFights, getEventWithUserPicks } from './events';
+export { getGlobalRanking, getMonthlyRanking, getAvailableMonths } from './ranking';
